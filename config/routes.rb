3e425e2ca033e94a689214cbc15ff3aad6029840
Wojtek Kostanski <wojtek@tappest.com>
2018-10-05 10:35:58 +0200

    add layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'page/index'
   root 'page#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', to: redirect('/')
 end
