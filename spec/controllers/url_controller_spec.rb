413002bdd42298538f48024f32f6570f814d8c84
wojtek <wojtek.kostanski@gmail.com>
2018-11-04 00:07:48 +0100

    fix rubocop offences

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UrlsController do
   describe 'POST create' do
-    let(:valid_params) { {'url' => {'url_orginal' => 'http://wkostanski.pl'} } }
-    let(:invalid_params) { {'url' => {'url_orginal' => 'wkostanski.pl'} } }
+    let(:valid_params) { {'url' => {'url_orginal' => 'http://wkostanski.pl'}} }
+    let(:invalid_params) { {'url' => {'url_orginal' => 'wkostanski.pl'}} }
     it 'with valid params' do
-      expect{
+      expect do
         post :create, params: valid_params
-      }.to change(Url, :count).by(1)
+      end.to change(Url, :count).by(1)
     end
     it 'with invalid params' do
-      expect{
+      expect do
         post :create, params: invalid_params
-      }.to change(Url, :count).by(0)
+      end.to change(Url, :count).by(0)
     end
     it 'with duplicated params' do
       post :create, params: valid_params
-      expect{
+      expect do
         post :create, params: valid_params
-      }.to change(Url, :count).by(0)
+      end.to change(Url, :count).by(0)
     end
   end
 end
