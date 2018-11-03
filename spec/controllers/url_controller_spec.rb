89bbeb60cec4bd6eeefcb8fa025810e358683f5a
wojtek <wojtek.kostanski@gmail.com>
2018-11-03 23:46:09 +0100

    test controller

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -1,27 +1,24 @@
-# describe "POST create" do
-#   context "with valid attributes" do
-#     it "creates a new url" do
-#       expect{
-#         post :create, url: Factory.attributes_for(:url)
-#       }.to change(Url,:count).by(1)
-#     end
+require 'rails_helper'
 
-#     it "redirects to the new url" do
-#       post :create, url: Factory.attributes_for(:url)
-#       response.should redirect_to Url.last
-#     end
-#   end
-
-#   context "with invalid attributes" do
-#     it "does not save the new url" do
-#       expect{
-#         post :create, url: Factory.attributes_for(:invalid_url)
-#       }.to_not change(Url,:count)
-#     end
-
-#     it "re-renders the new method" do
-#       post :create, url: Factory.attributes_for(:invalid_url)
-#       response.should render_template :new
-#     end
-#   end 
-# end
+RSpec.describe UrlsController do
+  describe 'POST create' do
+    let(:valid_params) { {'url' => {'url_orginal' => 'http://wkostanski.pl'} } }
+    let(:invalid_params) { {'url' => {'url_orginal' => 'wkostanski.pl'} } }
+    it 'with valid params' do
+      expect{
+        post :create, params: valid_params
+      }.to change(Url, :count).by(1)
+    end
+    it 'with invalid params' do
+      expect{
+        post :create, params: invalid_params
+      }.to change(Url, :count).by(0)
+    end
+    it 'with duplicated params' do
+      post :create, params: valid_params
+      expect{
+        post :create, params: valid_params
+      }.to change(Url, :count).by(0)
+    end
+  end
+end
